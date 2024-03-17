bbed4219c478c4f3e2189ef71cfc60c1ccc71601
Brandon3964 <77771338+Brandon3964@users.noreply.github.com>
2024-03-17 15:11:26 -0700

    Update sphinx_wino.py

--- sphinx_wino.py
+++ sphinx_wino.py
@@ -11,7 +11,6 @@ For the provided image and its associated question, generate only a scene graph 
 3. Object relationships that are relevant to answering the question
 '''
 
-
 model = SPHINXModel.from_pretrained(pretrained_path="", with_visual=True)
 
 
@@ -21,7 +20,6 @@ result_path = ""
 result_file = open(result_path, 'w')
 
 
-
 with open(question_path, 'r') as json_file:
     json_list = list(json_file)
 
@@ -29,9 +27,9 @@ with open(question_path, 'r') as json_file:
 for json_str in tqdm(json_list):
 
     cur_pair = json.loads(json_str)
-
     cur_image = Image.open(image_file + cur_pair["image"] + ".png")
     cur_caption = cur_pair["caption"]
+
     cur_question = [["Does the given caption accurately describe the given image? Caption:" + cur_caption + ".\n\n" + sgPrompt, None]]
     cur_sg = model.generate_response(cur_question, cur_image, max_gen_len=256, temperature=0)
 
@@ -40,9 +38,7 @@ for json_str in tqdm(json_list):
 
 
 
-    stored_response = {}
-    stored_response["text"] = final_ans
-
+    stored_response = {"text":final_ans}
     result_file.write(json.dumps(stored_response) + "\n")
     result_file.flush()
 
