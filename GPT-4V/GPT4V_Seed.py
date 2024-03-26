65324d82859f4e55974963b17779e84b2285f686
Brandon3964 <77771338+Brandon3964@users.noreply.github.com>
2024-03-25 21:06:23 -0700

    Update GPT4V_Seed.py

--- GPT-4V/GPT4V_Seed.py
+++ GPT-4V/GPT4V_Seed.py
@@ -7,10 +7,10 @@ from openai import OpenAI
 import json
 
 image_file = '' # Image Path
-question_path = ""
-result_path = ""
+question_path = "" #Question Path
+result_path = "" #File to store result
 result_file = open(result_path, 'w')
-api_key=''
+api_key='' #Openai api key
 
 sgPrompt='''
 For the provided image and its associated question, generate only a scene graph in JSON format that includes the following:
@@ -18,13 +18,37 @@ For the provided image and its associated question, generate only a scene graph 
 2. Object attributes that are relevant to answering the question
 3. Object relationships that are relevant to answering the question
 '''
-answerPrompt=".Use the image and scene graph as context and answer the following question: "
 
 
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
+
+def create_payload(cur_image, cur_text):
+    payload = {
+    "model": "gpt-4-vision-preview",
+    "messages": [
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{cur_image}"
+            }
+            },
+            {
+            "type": "text",
+            "text": cur_text
+            }
+        ]
+        }
+    ],
+    "max_tokens": 512,
+    "temperature":0
+    }
+  return payload
 
 
 with open(question_path, 'r') as json_file:
@@ -49,72 +73,21 @@ for json_str in tqdm(json_list):
             cur_question = result["text"]
             cur_id = result["question_id"]
 
-
-            payload = {
-            "model": "gpt-4-vision-preview",
-            "messages": [
-                {
-                "role": "user",
-                "content": [
-                    {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{cur_image}"
-                    }
-                    },
-                    {
-                    "type": "text",
-                    "text": "Question: " + cur_question.split("?")[0] + "?" + sgPrompt
-                    }
-                ]
-                }
-            ],
-            "max_tokens": 512,
-            "temperature":0
-            }
-
+            payload = create_payload(cur_image, "Question: " + cur_question.split("?")[0] + "?" + sgPrompt)
             response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
             cur_sg = response.json()["choices"][0]["message"]["content"]
 
-            new_q = "Scene Graph: " + cur_sg + answerPrompt + cur_question
-
-            payload = {
-            "model": "gpt-4-vision-preview",
-            "messages": [
-                {
-                "role": "user",
-                "content": [
-                    {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{cur_image}"
-                    }
-                    },
-                    {
-                    "type": "text",
-                    "text": new_q
-                    },
-                ]
-                }
-            ],
-            "max_tokens": 512,
-            "temperature":0
-            }
+            new_q = f"Scene Graph: {cur_sg}. Use the image and scene graph as context and answer the following question: {cur_question}"
+            payload = create_payload(cur_image, new_q)
 
             response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
             final_ans = response.json()["choices"][0]["message"]["content"]
 
 
-
-            q0 = {}
-            q0["question_id"] = cur_id
-            q0["text"] = final_ans
-
-            result_file.write(json.dumps(q0) + "\n")
-            result_file.flush()
+            temp_result = {"question_id":cur_id, "text":final_ans}
+            result_file.write(json.dumps(temp_result) + "\n")
             is_done = True
         except:
-
             fail_count += 1
             if fail_count == 5:
                 break
