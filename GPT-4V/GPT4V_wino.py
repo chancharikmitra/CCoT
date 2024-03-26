0f8e6beb13d90830b4f0b5c6251b506b6f1f9951
Brandon3964 <77771338+Brandon3964@users.noreply.github.com>
2024-03-25 21:43:06 -0700

    Update GPT4V_wino.py

--- GPT-4V/GPT4V_wino.py
+++ GPT-4V/GPT4V_wino.py
@@ -7,10 +7,10 @@ from openai import OpenAI
 import json
 
 image_file = "" # Image Path
-question_path = ""
-result_path = ""
+question_path = "" #Path to question
+result_path = "" #Path to store result
 result_file = open(result_path, 'w')
-api_key=""
+api_key="" #Openai api key
 
 sgPrompt='''
 For the provided image and its associated question, generate only a scene graph in JSON format that includes the following:
@@ -20,10 +20,41 @@ For the provided image and its associated question, generate only a scene graph 
 '''
 answerPrompt=".Use the image and scene graph as context and answer the following question: "
 
+def create_payload(cur_image, cur_text):
+    payload = {
+    "model": "gpt-4-vision-preview",
+    "messages": [
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{cur_image}"
+            }
+            },
+            {
+            "type": "text",
+            "text": cur_text
+            }
+        ]
+        }
+    ],
+    "max_tokens": 512,
+    "temperature":0
+    }
+  return payload
+
 
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+headers = {
+"Content-Type": "application/json",
+"Authorization": f"Bearer {api_key}"
+}
 
 
 with open(question_path, 'r') as json_file:
@@ -37,81 +68,27 @@ for json_str in tqdm(json_list):
     while not is_done:
         try:
             result = json.loads(json_str)
-
             cur_image = encode_image(image_file + result["image"] + ".png")
             cur_caption = result["caption"]
-            cur_question = "Does the given caption accurately describe the given image? Caption:" + cur_caption + ".\n\n" + sgPrompt
+            cur_question = f"Does the given caption accurately describe the given image? Caption:{cur_caption}\n\n{sgPrompt}"
 
-
-            headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {api_key}"
-            }
-
-            payload = {
-            "model": "gpt-4-vision-preview",
-            "messages": [
-                {
-                "role": "user",
-                "content": [
-                    {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{cur_image}"
-                    }
-                    },
-                    {
-                    "type": "text",
-                    "text": cur_question
-                    }
-                ]
-                }
-            ],
-            "max_tokens": 512,
-            "temperature": 0
-            }
-
+            payload = create_payload(cur_image, cur_question)
             response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
             cur_sg = response.json()["choices"][0]["message"]["content"]
-            new_q = "Question: Does the given caption accurately describe the given image? Caption:" +  cur_caption + ".\n\nAnswer: Scene graph:" + cur_sg + '''\n\nUse the scene graph and image to reason and answer the question:'''
 
-
-            payload = {
-            "model": "gpt-4-vision-preview",
-            "messages": [
-                {
-                "role": "user",
-                "content": [
-                    {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{cur_image}"
-                    }
-                    },
-                    {
-                    "type": "text",
-                    "text": new_q
-                    },
-                ]
-                }
-            ],
-            "max_tokens": 512,
-            "temperature": 0
-            }
-
+          
+            new_q = f"Question: Does the given caption accurately describe the given image? Caption:{cur_caption}.\n\nAnswer: Scene graph:{cur_sg}\n\nUse the scene graph and image to reason and answer the question:"
+            payload = create_payload(cur_image, new_q)
             response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
             final_ans = response.json()["choices"][0]["message"]["content"]
 
 
-            stored_response = {"text":final_ans}
-            result_file.write(json.dumps(stored_response) + "\n")
-            result_file.flush()
+            temp_result = {"text":final_ans}
+            result_file.write(json.dumps(temp_result) + "\n")
             is_done = True
         except:
-
             fail_count += 1
             if fail_count == 5:
                 break
             is_done = False
-
 result_file.close()
